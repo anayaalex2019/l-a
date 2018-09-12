7c48bbdbffc09cc1116fab2c2195438db6319201
anayaalex2019 <40498454+anayaalex2019@users.noreply.github.com>
2018-09-12 09:07:47 -0700

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("l(a \n \n \nle \n \naf \n\nfa \n \n \nll \n \n \ns) \n \none \n \nl \n \n \niness");
-    }
-}
